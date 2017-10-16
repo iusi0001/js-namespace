14bfe2c263f95885b695aea79e9f5ff9b6b911be
iusi0001 <32827417+iusi0001@users.noreply.github.com>
2017-10-16 00:41:02 -0400

    Update namespace.js

--- namespace.js
+++ namespace.js
@@ -2,6 +2,7 @@ var IUSI0001= {
     
     init: function(){
         var box = document.createElement("div");
+        box.classList.add("box");
         box.textContent = "IUSI0001";
         let boxes = document.getElementById('boxes');
         boxes.appendChild(box);
